0c22cb3dd8b89e3b02248be92db21075900634cf
sivasaikiran <msskiran2020@gmail.com>
2025-07-04 11:19:14 +0530

    Initial commit

--- Backend/config/socket.js
+++ Backend/config/socket.js
@@ -8,7 +8,7 @@ const setupSocket = (server) => {
 
     const io = new Server(server, {
         cors: {
-            origin: ["http://localhost:5173", "https://zap-chat-six.vercel.app"],
+            origin: ["http://localhost:5173"],
             credentials: true,
         },
     })
